b933fdce3a9f6391c4d152057c5a8992144dedbe
Conor Leonard <150186349+ConorDaniel@users.noreply.github.com>
2025-03-24 20:27:19 +0000

    Delete src directory

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from "./server-instance.js";
-
-async function init() {
-  const server = await createServer();
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-}
-
-init();
